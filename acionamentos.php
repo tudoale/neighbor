12f94cd8172b76295f505a33324e2d9d14a13c3b
Alessandro F <34175788+tudoale@users.noreply.github.com>
2023-10-04 21:26:50 -0300

    Add files via upload

---
+++ acionamentos.php
@@ -0,0 +1,32 @@
+<?php
+include ('conexao.php'); //$tabela
+ 
+// conexão e seleção do banco de dados
+$con = mysqlI_connect($host, $user, $pass, $db);
+$id=1;
+$tempo = $_GET['t'];
+$botao = $_GET['b'];
+$acionamento = $_GET['a'];
+$status_botao = $_GET['s'];
+$botao_r = "botao_".$botao;
+$status_b = "status_bt".$status_botao;
+//echo $botao_r." = ".$acionamento;
+
+if($botao >= 1 & $botao <= 2) {
+	$results = "UPDATE $tabela SET $botao_r = '$acionamento' WHERE id='$id'";
+	$resultado_configs = mysqli_query($con, $results);
+}
+
+if($status_botao >= 1 & $status_botao <= 2) {
+	$results = "UPDATE $tabela SET $status_b  = '$acionamento' WHERE id='$id'";
+	$resultado_configs = mysqli_query($con, $results);
+}
+
+if($tempo > 0 & $tempo < 101) {
+	$results = "UPDATE $tabela SET tempo  = '$tempo' WHERE id='$id'";
+	$resultado_configs = mysqli_query($con, $results);
+}
+
+ // fecha a conexão
+mysqli_close($con);
+?>
---
+++ status_acionamento.php
@@ -0,0 +1,32 @@
+<?php
+
+include ('conexao.php'); //$tabela
+ 
+// conexão e seleção do banco de dados
+$con = mysqlI_connect($host, $user, $pass, $db);
+ 
+// executa a consulta
+$sql = "SELECT * FROM $tabela ORDER BY id";
+$res = mysqli_query($con, $sql);
+ 
+// conta o número de registros
+$total = mysqli_num_rows($res);
+ 
+//echo "<p>Total de Resultados: " . $total . "</p>";
+//$datahora= date("d/m/Y h:i:s");
+ 
+// loop pelos registros
+if ($f = mysqli_fetch_array($res))
+{
+    $botao_1= $f['botao_1'];
+	$botao_2= $f['botao_2'];
+	$status_bt1= $f['status_bt1'];
+	$status_bt2= $f['status_bt2'];
+	$tempo = $f['tempo'];
+	
+	echo $botao_1.$botao_2.$tempo ;
+}
+ 
+// fecha a conexão
+mysqli_close($con);
+?>
